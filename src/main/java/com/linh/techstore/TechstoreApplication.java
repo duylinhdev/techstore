4b73470f29f326a59f2d8e3e15afef31fc2868cf
Le Duy Linh <lduylinh318@gmail.com>
2026-08-30 14:31:19 +0700

    feat(init): initialize Spring Boot techstore  project structure

---
+++ src/main/java/com/linh/techstore/TechstoreApplication.java
@@ -0,0 +1,14 @@
+package com.linh.techstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechstoreApplication.class, args);
+    }
+    
+
+}
---
+++ src/main/java/com/linh/techstore/controllers/MainController.java
@@ -0,0 +1,52 @@
+package com.linh.ecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/dang-nhap")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/san-pham")
+    public String showProducts() {
+        return "shop";
+    }
+
+    @GetMapping("/lien-he")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/gioi-thieu")
+    public String showAbout() {
+        return "about";
+    }
+
+    @GetMapping("/yeu-thich")
+    public String showWishlist() {
+        return "wishlist";
+    }
+
+    @GetMapping("/gio-hang")
+    public String showCart() {
+        return "cart";
+    }
+
+    @GetMapping("/thanh-toan")
+    public String showCheckout() {
+        return "checkout";
+    }
+
+    @GetMapping("/tai-khoan")
+    public String showAccount() {
+        return "my-account";
+    }
+}
