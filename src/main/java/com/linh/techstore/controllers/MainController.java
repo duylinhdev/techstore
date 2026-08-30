ce1e4a3eaf0ef49d75935ed0a6ab66a29115f361
Le Duy Linh <lduylinh318@gmail.com>
2026-08-30 14:40:45 +0700

    refactor(controller): update package declaration to com.linh.techstore.controllers

--- src/main/java/com/linh/techstore/controllers/MainController.java
+++ src/main/java/com/linh/techstore/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.linh.ecommerce.controllers;
+package com.linh.techstore.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
